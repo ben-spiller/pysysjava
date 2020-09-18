802da84c66ea7b99d7570ff057c55bfd8c26ecb0
ben-spiller <ben-spiller@users.noreply.github.com>
2020-09-18 22:19:37 +0100

    add Python packaging

--- README.rst
+++ README.rst
@@ -1,17 +1,18 @@
-PySys plugins for Java
+.. image:: ../../workflows/PySys/badge.svg
+	:target: ../../actions
+.. image:: ../../workflows/Docs/badge.svg
+	:target: ../../actions
+.. image:: https://codecov.io/gh/ben-spiller/pysysjava/branch/main/graph/badge.svg
+	:target: https://codecov.io/gh/ben-spiller/pysysjava
+
+Java plugins for PySys
 ======================
 This is a collection of plugins that add support for compiling, running and testing Java(R) classes using the 
 PySys System Test Framework.
 
-.. inclusion-marker-section-project-links
+Requires:
+	- Python 3.6+
+	- PySys 1.6.1+
+	- Java 8+ (currently tested with Java 8 and Java 14)
 
-.. image:: ../../workflows/PySys/badge.svg
-	:target: ../../actions
-
-.. image:: ../../workflows/Docs/badge.svg
-	:target: ../../actions
-
-.. image:: https://codecov.io/gh/ben-spiller/pysysjava/branch/main/graph/badge.svg
-	:target: https://codecov.io/gh/ben-spiller/pysysjava
-
-See https://ben-spiller.github.io/pysysjava/.
+See the documentation at https://ben-spiller.github.io/pysysjava/.
---
+++ setup.py
@@ -0,0 +1,84 @@
+#!/usr/bin/env python3
+
+import codecs, os, glob, sys, shutil
+
+ROOTDIR = os.path.abspath(os.path.dirname(__file__))
+
+import setuptools
+print('using setuptools v%s'%setuptools.__version__)
+from setuptools import setup, find_packages
+
+# Classifiers come from PyPi's official list https://pypi.org/classifiers/
+PLATFORMS_CLASSIFIERS = [
+	"Operating System :: OS Independent",
+	"Operating System :: Microsoft :: Windows",
+	"Operating System :: POSIX :: Linux",
+	"Operating System :: MacOS",
+]
+LICENSE_CLASSIFIER = "License :: Public Domain"
+
+CLASSIFIERS = [
+	LICENSE_CLASSIFIER,
+	"Development Status :: 4 - Beta",
+
+	"Intended Audience :: Developers",
+	"Environment :: Plugins",
+
+	"Intended Audience :: Developers",
+	"Topic :: Software Development :: Quality Assurance",
+	"Topic :: Software Development :: Testing",
+	"Topic :: Software Development :: Testing :: Unit",
+	"Topic :: Education :: Testing",
+
+	"Programming Language :: Python",
+	"Programming Language :: Python :: 3.6", 
+	"Programming Language :: Python :: 3.7", 
+	"Programming Language :: Python :: 3.8", # see also python_requires
+	"Programming Language :: Python :: Implementation :: CPython",
+
+	"Natural Language :: English",
+]+PLATFORMS_CLASSIFIERS
+KEYWORDS = ['pysys', 'java', 'junit']
+
+with codecs.open(ROOTDIR+'/README.rst', "r", encoding="ascii") as f:
+	long_description = f.read()
+	# Strip out the current build status badges from the top of the readme
+	long_description = long_description[long_description.find(os.linesep*2):].strip()
+	assert len(long_description)>100, repr(long_description)
+
+with codecs.open(ROOTDIR+'/VERSION', "r", encoding="ascii") as f:
+	version = f.read().strip()
+
+REPO_URL = "https://github.com/ben-spiller/pysysjava"
+
+setup(
+	name='PySys-Java',
+	description='Java plugins for the PySys System Test Framework',
+	
+	url=REPO_URL,
+	project_urls={ 
+		'Repository': REPO_URL,
+		'Documentation': 'https://ben-spiller.github.io/pysysjava/',
+	},
+
+	author='Ben Spiller', 
+
+	version=version,
+	license=LICENSE_CLASSIFIER.split('::')[-1].strip(),
+	keywords=KEYWORDS,
+	classifiers=CLASSIFIERS,
+	platforms=PLATFORMS_CLASSIFIERS,
+	long_description=long_description,
+	long_description_content_type='text/x-rst',
+
+	python_requires=">=3.6, <4",
+
+	install_requires=[
+		"PySys >= 1.6.1"
+	],
+	
+	packages=setuptools.find_packages(),
+	include_package_data=True,
+
+)
+	
